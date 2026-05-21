aa182145482e6c1e8783d62a8861aae497826a0d
UBoratti <urielboratti15@gmail.com>
2026-05-20 23:06:28 -0300

    Se agrego el menu

--- main.py
+++ main.py
@@ -12,3 +12,43 @@ if not paises:
     {"nombre": "Brasil",    "poblacion": 213993437, "superficie": 8515767.0, "continente": "America"},
     {"nombre": "Alemania",  "poblacion": 83149300,  "superficie": 357022.0,  "continente": "Europa"},
   ]
+
+#Menu principal
+
+while True:
+    print("""\n=======Menu principal======
+1.Agregar pais
+2.Listar paises
+3.Buscar pais
+4.Modificar pais
+5.Eliminar pais
+6.Filtar pais
+7.Ordenar paises
+8.Estadisticas
+9.Salir
+===============================""")
+
+    opcion = input("Ingrese una opcion: ").strip()
+
+    match opcion:
+        case "1":
+            paises = agregar_pais(paises)
+        case "2":
+            listar_paises(paises)
+        case "3":
+            buscar_pais(paises)
+        case "4":
+            paises = modificar_pais(paises)
+        case "5":
+            paises = eliminar_pais(paises)
+        case "6":
+            filtral_paises(paises)
+        case "7":
+            ordena_paises(paises)
+        case "8":
+            mostar_estadisticas(paises)
+        case "9":
+            print("Saliendo del programa")
+            break
+        case _:
+            print("Opcion no valida")
