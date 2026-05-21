cb353aa7bc62fa8fea90dad8b2ac93671b93775a
santtu23 <santisecjalil12@gmail.com>
2026-05-20 22:53:25 -0300

    Se crean funciones de validacion

--- fcs.py
+++ fcs.py
@@ -53,3 +53,49 @@ def cargar_csv(ruta_archivo):
 
   print(f"{len(paises)} paises cargados correctamente '{ruta_archivo}'")
   return paises
+
+# ---------
+
+def validar_texto(mensaje_1, mensaje_2=None):
+    while True:
+      try:
+        texto = input(mensaje_1).strip()
+        # FIX: se permite espacio para nombres como "Nueva Zelanda"
+        if texto.replace(" ", "").isalpha():
+          if mensaje_2 != None:
+            print(mensaje_2)
+          return texto
+        else:
+          print("ERROR... El texto solo puede contener letras.")
+      except Exception as e:
+        print(f"Hubo un error inesperado... Error: {e}.")
+
+def validar_entero(mensaje_1, mensaje_2=None):
+  while True:
+    try:
+      numero = int(input(mensaje_1))
+      if numero < 0:
+        print("ERROR... No se permiten números negativos.")
+        continue
+      if mensaje_2 != None:
+        print(mensaje_2)
+      return numero
+    except ValueError:
+      print("ERROR... Por favor ingrese un número entero.")
+    except Exception as e:
+      print(f"Hubo un error inesperado... Error: {e}.")
+
+def validar_flotante(mensaje_1, mensaje_2=None):
+  while True:
+    try:
+      numero = float(input(mensaje_1))
+      if numero < 0:
+        print("ERROR... No se permiten números negativos.")
+        continue
+      if mensaje_2 != None:
+        print(mensaje_2)
+      return numero
+    except ValueError:
+      print("ERROR... Por favor ingrese un número válido.")
+    except Exception as e:
+      print(f"Hubo un error inesperado... Error: {e}.")
