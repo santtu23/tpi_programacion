0b6cd2a760bde879ac8aee38379067bb62f3ad3b
UBoratti <urielboratti15@gmail.com>
2026-05-22 19:38:09 -0300

    Se arreglo el problema del csv

--- main.py
+++ main.py
@@ -2,16 +2,19 @@ from fcs import *
 
 #Carga del csv
 
-paises = cargar_csv("paises.csv")
+ARCHIVO_CSV ="paises.csv"
+paises = cargar_csv(ARCHIVO_CSV)
 
 # Si no hay csv o esta vacio
 if not paises:
-  paises = [
-    {"nombre": "Argentina", "poblacion": 45376763, "superficie": 2780400, "continente": "America"},
-    {"nombre": "Japon", "poblacion": 125800000, "superficie": 377975, "continente": "Asia"},
-    {"nombre": "Brasil",    "poblacion": 213993437, "superficie": 8515767.0, "continente": "America"},
-    {"nombre": "Alemania",  "poblacion": 83149300,  "superficie": 357022.0,  "continente": "Europa"},
-  ]
+    paises = [
+        {"nombre": "Argentina", "poblacion": 45376763, "superficie": 2780400, "continente": "America"},
+        {"nombre": "Japon", "poblacion": 125800000, "superficie": 377975, "continente": "Asia"},
+        {"nombre": "Brasil",    "poblacion": 213993437, "superficie": 8515767.0, "continente": "America"},
+        {"nombre": "Alemania",  "poblacion": 83149300,  "superficie": 357022.0,  "continente": "Europa"},
+]
+
+guardar_csv(paises, ARCHIVO_CSV)
 
 #Menu principal
 
