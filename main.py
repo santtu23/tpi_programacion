76e99d7480c26f0cae4b12d03f9a6153679e5ed4
santtu23 <santisecjalil12@gmail.com>
2026-05-26 19:34:00 -0300

    Guardar cambios

--- main.py
+++ main.py
@@ -3,8 +3,7 @@ from fcs import *
 #Carga del csv
 
 ARCHIVO_CSV ="paises.csv"
-paises =[]
-paises = cargar_csv(ARCHIVO_CSV, paises)
+paises = cargar_csv(ARCHIVO_CSV)
 
 # Si no hay csv o esta vacio
 if not paises:
@@ -15,6 +14,7 @@ if not paises:
         {"nombre": "Alemania",  "poblacion": 83149300,  "superficie": 357022.0,  "continente": "Europa"},
 ]
 
+guardar_csv(paises, ARCHIVO_CSV)
 
 #Menu principal
 
